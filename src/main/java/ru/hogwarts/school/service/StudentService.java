a38f199bd1074f4510a2afe042cf185a4a248c0c
Anton S <hitman.soldirs@gmail.com>
2025-09-09 12:12:26 +0800

    migration and index. done HW

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.hogwarts.school.exception.StudentNotFoundException;
@@ -14,6 +15,7 @@ import java.nio.file.Path;
 import java.util.List;
 
 @Service
+@Slf4j
 public class StudentService {
 
     private final StudentRepository studentRepository;
@@ -25,31 +27,39 @@ public class StudentService {
     }
 
     public List<Student> findByFacultyId(Long facultyId) {
+        log.info("Ыфзван метод поиска студентов по факультету = {}", facultyId);
         return studentRepository.findByFacultyId(facultyId);
     }
 
     public Student createStudent(Student student) {
+        log.info("был вызван метод создания студента: {}", student);
         return studentRepository.save(student);
     }
 
     public Student getStudent(Long id) {
+        log.info("вызван метод получения студента по id = {}", id);
         return studentRepository.findById(id).orElse(null);
     }
 
     public Student updateStudent(Long id, Student student) {
+        log.info("Вызван метод корректировки студента для id = {} c новыми данными: {}", id, student);
         if (!studentRepository.existsById(id)) {
+            log.error("Не удалось обновить: студент с id = {} не найден", id);
             throw new StudentNotFoundException(id);
         }
         student.setId(id);
+        log.debug("студент обновлён");
         return studentRepository.save(student);
     }
 
     public List<Student> getAllStudents() {
+        log.info("Вызван метод показа всех студентов");
         return studentRepository.findAll();
     }
 
     @Transactional
-    public void deleteStudent(Long studentId) throws IOException {
+    public void deleteStudent(Long studentId) {
+        log.info("Вызван метод удаления студента для = {}", studentId);
         Student student = studentRepository.findById(studentId)
                 .orElseThrow(() -> new EntityNotFoundException("Студент с id " + studentId + " не найден"));
 
@@ -57,36 +67,49 @@ public class StudentService {
             try {
                 Path avatarPath = Path.of(avatar.getFilePath());
                 Files.deleteIfExists(avatarPath);
+                log.debug("Удалён файл аватара: {}", avatarPath);
             } catch (IOException e) {
+                log.error("Ошибка при удалении аватарки для студента = {}", studentId, e);
                 throw new RuntimeException("Не удалось удалить файл аватарки", e);
             }
             avatarRepository.delete(avatar);
+            log.debug("Удалена аватарка из БД: {}", avatar);
         });
 
         studentRepository.delete(student);
+        log.info("Студент с id = {}", studentId);
     }
 
 
     public List<Student> findByAge(int age) {
-        return studentRepository.findAll().
+        log.info("Вызван метод поиска по возрасту = {}", age);
+        List<Student> result = studentRepository.findAll().
                 stream()
                 .filter(student -> student.getAge() == age)
                 .toList();
+        log.info("Получен список студентов");
+        return result;
     }
 
     public List<Student> findByAgeBetween(int min, int max) {
-        return studentRepository.findByAgeBetween(min, max);
+        log.info("Вызван метод поиска студентов по промежутку возрастов: min={}, max={}", min, max);
+        List<Student> result = studentRepository.findByAgeBetween(min, max);
+        log.info("Получен список студентов  по промежутку возрастов: min={}, max={}", min, max);
+        return result;
     }
 
     public long getTotalStudents() {
+        log.info("Вызван метод показа колличества студентов");
         return studentRepository.getTotalStudents();
     }
 
     public double getAverageAge() {
+        log.info("Вызван метод подсчёта среднего возраста студентов");
         return studentRepository.getAverageAge();
     }
 
     public List<Student> getLAstFiveStudents() {
+        log.info("Вызван метод показа последжних пяти студентов");
         return studentRepository.getLastFiveStudents();
     }
 
